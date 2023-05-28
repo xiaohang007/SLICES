0876a3ec700c018e8c73b3cee0c5a0cdb401a5b1
Hang <33337564+xiaohang007@users.noreply.github.com>
2023-05-28 18:05:07 +0800

    Update rundocker.sh

--- rundocker.sh
+++ rundocker.sh
@@ -1,1 +1,1 @@
-docker run  -it -h workq --shm-size=0.1gb  -v /:/crystal -w /crystal crystal:80 /crystal/entrypoint_set_cpus.sh
+docker run  -it -h workq --shm-size=0.1gb  -v /[]:/crystal -w /crystal crystal:80 /crystal/entrypoint_set_cpus.sh
