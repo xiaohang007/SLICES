58d5ad8ec8549e03579f1e1ee471dc4eb9636005
Hang Xiao, 肖航 <xiaohang07@live.cn>
2024-02-02 16:15:38 +0800

    Update 2.1structure2SLICES_SLICES2structure.py

--- examples/2.1structure2SLICES_SLICES2structure.py
+++ examples/2.1structure2SLICES_SLICES2structure.py
@@ -9,6 +9,7 @@ slices_NdSiRu=backend.structure2SLICES(original_structure)
 # converting a SLICES string back to its original crystal structure and obtaining its M3GNet_IAP-predicted energy_per_atom
 reconstructed_structure,final_energy_per_atom_IAP = backend.SLICES2structure(slices_NdSiRu)
 print('SLICES string of NdSiRu is: ',slices_NdSiRu)
+print('\nOriginal_structure is: ',original_structure)
 print('\nReconstructed_structure is: ',reconstructed_structure)
 print('\nfinal_energy_per_atom_IAP is: ',final_energy_per_atom_IAP,' eV/atom')
 # if final_energy_per_atom_IAP is 0, it means the M3GNet_IAP refinement failed, and the reconstructed_structure is the ZL*-optimized structure.
