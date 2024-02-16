a481a51b32e71d0686ebb6b9edbf8d3e1b464081
Hang Xiao, 肖航 <xiaohang07@live.cn>
2024-02-16 16:30:40 +0800

    fix

--- HTS/7_EAH_prescreen/0_setup.sh
+++ HTS/7_EAH_prescreen/0_setup.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 rm result.csv
-python ehull.py -i "results_5_symmetry_filter_refine_filtered.csv" -o result.csv
+python ehull.py -i "../5_symmetry_filter_refine/results_5_symmetry_filter_refine_filtered.csv" -o result.csv
