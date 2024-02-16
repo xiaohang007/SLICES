82ece12fa6e6b203fdc051d7637e656d7d557128
Hang Xiao, 肖航 <xiaohang07@live.cn>
2024-02-16 16:20:35 +0800

    Update 0_setup.sh

--- HTS/7_EAH_prescreen/0_setup.sh
+++ HTS/7_EAH_prescreen/0_setup.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-source /home/yanchen/miniconda3/etc/profile.d/conda.sh
-conda activate alignn
+
 rm result.csv
 python ehull.py -i "results_5_symmetry_filter_refine_filtered.csv" -o result.csv
