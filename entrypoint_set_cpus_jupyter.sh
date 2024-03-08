bb79b5aa414ba8105fd11177080992a9366b0ac7
Hang Xiao, 肖航 <xiaohang07@live.cn>
2024-03-08 11:12:23 +0800

    Update entrypoint_set_cpus_jupyter.sh

--- entrypoint_set_cpus_jupyter.sh
+++ entrypoint_set_cpus_jupyter.sh
@@ -6,4 +6,4 @@ service slurmctld restart
 service slurmd restart
 sinfo
 
-jupyter notebook --allow-root --ip 0.0.0.0 --port=8888
+jupyter notebook --allow-root --ip 0.0.0.0 --port=8888 --NotebookApp.iopub_data_rate_limit=1.0e10
