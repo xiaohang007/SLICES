5162098b422e9e6d038fd83f1907a8317f1b4c17
Hang Xiao, 肖航 <xiaohang07@live.cn>
2024-05-10 17:07:13 +0800

    add old scripts for Tutorial 3

--- benchmark/Validity_rate_ucRNN__Success_rate_cRNN/2_conditioned_RNN/3_inverse/0_run.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Hang Xiao 2023.04
-# xiaohang07@live.cn
-from invcryrep.utils import temporaryWorkingDirectory,splitRun_csv,show_progress,collect_csv
-splitRun_csv(filename='../2_train_sample/sampled.sli',threads=16,skip_header=False)
-show_progress()
-collect_csv(output="results_3_inverse.csv", glob_target="job_*/result.csv",\
-            header='SLICES,formula,energy_per_atom,POSCAR\n',cleanup=True)
