53faf312616d6055a04bd0dd2e1b228bb0ba1dbe
Hang Xiao, 肖航 <xiaohang07@live.cn>
2025-01-07 20:42:49 +0800

    update version number of setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 
-VERSION = '2.0.7'
+VERSION = '2.0.8'
 DESCRIPTION = "SLICES"
 
 
